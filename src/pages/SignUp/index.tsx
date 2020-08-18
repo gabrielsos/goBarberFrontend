837bfbedf1124e742b9d1fbc2c41d9795c1755ae
Gabriel Silva <gabrielsos99@gmail.com>
2020-08-18 00:13:32 -0300

    initial commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback, useRef } from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErros from '../../utils/getValidationErrors';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content, AnimationContainer, Background } from './styles';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
+import { useToast } from '../../hooks/toast';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback( async(data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um E-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos')
+      });
+
+      await schema.validate(data, {
+        abortEarly: false
+      });
+
+      api.post('/users', data);
+
+      history.push('/');
+
+      addToast({
+        type: 'sucess',
+        title: 'Cadastro realizado!',
+        description: 'Você já pode fazer seu logon no GoBarber!'
+      })
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErros(err)
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+      addToast({
+        type: 'error',
+        title: 'Erro no cadastro',
+        description: 'Ocorreu um erro ao fazer cadastro, tente novamente.',
+      });
+    }
+  }, [addToast, history]);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber"/>
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
+            <Input icon={ FiUser } name="name" placeholder="Nome" />
+            <Input icon={ FiMail } name="email" placeholder="E-mail" />
+
+            <Input icon={ FiLock } name="password" type="password" placeholder="Senha" />
+
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para logon
+          </Link>
+        </AnimationContainer>
+      </Content>
+    </Container>
+  );
+}
+
+export default SignUp
